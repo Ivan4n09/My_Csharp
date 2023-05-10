2fc8a985e86691b3d56a0970138ec0c8a8413923
Ivan <iss04@yandex.ru>
2023-05-10 21:35:18 +0300

    Added Code01 the most basic interval calculator

---
+++ Code01_ScalesCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int interval = Prompt("Input musical interval >");
+
+int calculatedNotePosition = 0 + interval;
+
+Console.WriteLine();
+
+string[] noteNames = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+string calculatedNote = noteNames[calculatedNotePosition % 12];
+
+System.Console.WriteLine("The note " + interval + " halfsteps away from C is " + calculatedNote + ".");
---
+++ Code01_ScalesCalculator/README.md
@@ -0,0 +1,1 @@
+The very basic musical note calculator
